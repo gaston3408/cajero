7bd89ec8d4914d5d38683473919362fb5486a8cc
unknown <you@example.com>
2019-07-17 18:36:33 -0300

    home banking

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <cajero/>
+  </div>
+</template>
+
+<script>
+import Cajero from './components/Cajero.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Cajero
+  }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro');
+  body {
+    margin: 0;
+
+    font-family: "Source Sans Pro", sans-serif;
+
+    background-color: #2a333d;
+}
+</style>
